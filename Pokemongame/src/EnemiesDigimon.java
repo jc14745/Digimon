f718f54ecc6bda59518ca7e28426172e0a4cd5a3
tonylieu1 <42420454+tonylieu1@users.noreply.github.com>
2019-03-12 20:27:16 -0400

    so i cant figure out how to set the relationship for it

--- Pokemongame/src/EnemiesDigimon.java
+++ Pokemongame/src/EnemiesDigimon.java
@@ -1,19 +1,29 @@
 
-public class EnemiesDigimon {
+public class EnemiesDigimon  {
 	int NumOfEnemies=100;
 	int NumOfStored;
-	Creature[] Enemies = new Creature[NumOfEnemies];
-	public void StoreEnemies(Creature C) {
-		if(Enemies[NumOfStored]== null) {
-			Enemies[NumOfStored]=C;
+	EnemiesDigimonCreation leftChild;
+	EnemiesDigimonCreation rightChild;
+	EnemiesDigimonCreation root;
+	EnemiesDigimonCreation[] Enemies = new EnemiesDigimonCreation[NumOfEnemies];
+	public void StoreEnemies(EnemiesDigimonCreation C) {
+		EnemiesDigimonCreation Enemiesdigimon = new EnemiesDigimonCreation();
+		if(root == null) {
+			root = C;
+			Enemies[NumOfStored]= root;	
 		}
 		else {
 			if(Enemies[NumOfStored].level < C.level && Enemies[(NumOfStored*2)+1]== null) {
-				Enemies[(NumOfStored*2)+1] = C; 
+				Enemiesdigimon = leftChild;
+				Enemiesdigimon.setArr((NumOfStored*2)+1);
+				Enemies[Enemiesdigimon.getNumber()] = Enemiesdigimon;
+				
 			}
 			else {
 				if(Enemies[NumOfStored].level < C.level && Enemies[(NumOfStored*2)+2]== null) {
-					Enemies[(NumOfStored*2)+2] = C; 
+					Enemiesdigimon = rightChild;
+					Enemiesdigimon.setArr((NumOfStored*2)+2);
+					Enemies[Enemiesdigimon.getNumber()] = Enemiesdigimon; 
 				}
 			}
 		}
@@ -26,7 +36,7 @@ public class EnemiesDigimon {
 		for(int pokemon = 0; pokemon < Enemies.length; pokemon++) {
 				for(int pokemonCompare = pokemon+1; pokemonCompare < Enemies.length;pokemonCompare++) {
 			if(Enemies[pokemon].level > Enemies[pokemonCompare].level) {
-				Creature temp = null;
+				EnemiesDigimonCreation temp = null;
 				temp = Enemies[pokemon];
 				Enemies[pokemon] = Enemies[pokemonCompare];
 				Enemies[pokemon+1] = temp;
---
+++ Pokemongame/src/EnemiesDigimonCreation.java
@@ -0,0 +1,63 @@
+
+public class EnemiesDigimonCreation {
+	String name;
+	int speed;
+	int attack;
+	int defense;
+	int HP;
+	int level;
+	int rank;
+	EnemiesDigimonCreation leftChild;
+	EnemiesDigimonCreation rightChild;
+	EnemiesDigimonCreation root;
+	public EnemiesDigimonCreation() {
+		
+	}
+	public EnemiesDigimonCreation(String name) {
+		this.name = name;
+	}
+	public EnemiesDigimonCreation(String name, int speed, int attack, int defense, int HP) {
+		this.name = name;
+		this.speed = speed;
+		this.attack = attack;
+		this.defense = defense;
+		this.HP = HP;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getHP() {
+		return HP;
+	}
+	public void setHP(int hP) {
+		HP = hP;
+	}
+	public void setArr(int number) {
+		rank = number;
+	}
+	public int getNumber() {
+		return rank;
+	}
+	
+}
