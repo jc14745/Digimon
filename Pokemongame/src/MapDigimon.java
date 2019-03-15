05790a4244a27596a01fe8117b6637decb015d2f
tonylieu1 <42420454+tonylieu1@users.noreply.github.com>
2019-03-15 16:56:26 -0400

    hellooo it me A digipoke

--- Pokemongame/src/MapDigimon.java
+++ Pokemongame/src/MapDigimon.java
@@ -5,67 +5,104 @@ public class MapDigimon {
 	//* the Lv should look like Lv1A the A will represent the pokemon like
 	//agumon will be A and be lv 1 and pikachu will be B and also lv 1
 	//agumon = 11 and pikachu = 12
-	Map<Integer, Creature> Digimon = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon1 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon2 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon3 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon4 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon5 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon6 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon7 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon8 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon9 = new HashMap<Integer, Creature>();
+	Map<Integer, Creature> Digimon10 = new HashMap<Integer, Creature>();
+		int lv= 1;
+		int lv2 = 1;
+		int lv3 = 1;
+		int lv4 = 1;
+		int lv5 = 1;
+		int lv6 = 1;
+		int lv7 = 1;
+		int lv8 = 1;
+		int lv9 = 1;
+		int lv10 = 1;
 	int MaxNumberOfE;
-	public void put( Creature C) {
-		int lv = 1;
-		int lv2 = 11;
-		int lv3 = 21;
-		int lv4 = 31;
-		int lv5 = 41;
-		int lv6 = 51;
-		int lv7 = 61;
-		int lv8 = 71;
-		int lv9 = 81;
-		int lv10 = 91;
+	public void put(Creature C) {
+	
 		if(C.getLevel()<10) {
-			Digimon.put(lv,C);
+			Digimon1.put(lv,C);
 			lv++;
 		}
 		else if(C.getLevel()<20) {
-			Digimon.put(lv2, C);
+			Digimon2.put(lv2, C);
 			lv2++;
 		}
 		else if(C.getLevel()<30) {
-			Digimon.put(lv3, C);
+			Digimon3.put(lv3, C);
 			lv3++;
 		}
 		else if(C.getLevel()<40) {
-			Digimon.put(lv4, C);
+			Digimon4.put(lv4, C);
 			lv4++;
 		}
 		else if(C.getLevel()<50) {
-			Digimon.put(lv5, C);
+			Digimon5.put(lv5, C);
 			lv5++;
 		}
 		else if(C.getLevel()<60) {
-			Digimon.put(lv6, C);
+			Digimon6.put(lv6, C);
 			lv6++;
 		}
 		else if(C.getLevel()<70) {
-			Digimon.put(lv7, C);
+			Digimon7.put(lv7, C);
 			lv7++;
 		}
 		else if(C.getLevel()<80) {
-			Digimon.put(lv8, C);
+			Digimon8.put(lv8, C);
 			lv8++;
 		}
 		else if(C.getLevel()<90) {
-			Digimon.put(lv9, C);
+			Digimon9.put(lv9, C);
 			lv9++;
 		}
 		else if(C.getLevel()<100) {
-			Digimon.put(lv10, C);
+			Digimon10.put(lv10, C);
 			lv10++;
 		}
 	}
-	public void search(int lv) {
-		Digimon.get(random(lv));
+	public void search(String name) {
+		if(name.contains("1")){
+			Digimon1.get(random(lv));
+		}
+		else if(name.contains("2")) {
+			Digimon2.get(random(lv2));
+		}
+		else if(name.contains("3")) {
+			Digimon3.get(random(lv3));
+		}
+		else if(name.contains("4")) {
+			Digimon4.get(random(lv4));
+		}
+		else if(name.contains("5")) {
+			Digimon5.get(random(lv5));
+		}
+		else if(name.contains("6")) {
+			Digimon6.get(random(lv6));
+		}
+		else if(name.contains("7")) {
+			Digimon7.get(random(lv7));
+		}
+		else if(name.contains("8")) {
+			Digimon8.get(random(lv8));
+		}
+		else if(name.contains("9")) {
+			Digimon9.get(random(lv9));
+		}
+		else if(name.contains("10")) {
+			Digimon10.get(random(lv10));
+		}
 	}
 	public int random(int lv) {
-		int maxNumber = lv*10;
-		int minNumber = maxNumber -9;
-		int Random = (int) (Math.random()*maxNumber+ minNumber);
+		int Random = (int) (Math.random()*lv+ 1);
 		return Random;
 	}
 }
